694334fe9c12a78bf6415b7cf6e942e591442a07
Maxwel <maxwellbakora@gmail.com>
2024-04-01 19:14:42 +0300

    Added question3

--- question3.js
+++ question3.js
@@ -1,4 +1,13 @@
-function primeNumbers(arrnumbers) {
-    let numbers = [];
-    
-}
+function primeNumbers(arr) {
+    return arr.filter(num => isPrime(num));
+  }
+  
+  function isPrime(num) {
+    if (num < 2) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i === 0) return false;
+    }
+    return true;
+  }
+  
+  console.log(primeNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
